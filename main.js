621a65e88c2d80b38ea7c22504f000f920e4e003
Abduhmid2012 <abusaxiymuhammadaliyev2@gmail.com>
2025-03-11 15:44:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+function yozfoiz(){
+    console.log("%")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "%";
+}
+function yozc(){
+    console.log("c")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "c";
+}
+function yozqavs(){
+    console.log("<")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "<";
+}
+function yoztayoq(){
+    console.log("/")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "/";
+}
+function yoz7(){
+    console.log("7")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "7";
+}
+function yoz8(){
+    console.log("8")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "8";
+}
+function yoz9(){
+    console.log("9")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "9";
+}
+function yozkupaytir(){
+    console.log("x")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "x";
+}
+function yoz4(){
+    console.log("4")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "4";
+}
+function yoz5(){
+    console.log("5")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "5";
+}
+function yoz6(){
+    console.log("6")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "6";
+}
+function yozminus(){
+    console.log("-")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "-";
+}
+function yoz1(){
+    console.log("1")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "1";
+}
+function yoz2(){
+    console.log("2")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "2";
+}
+function yoz3(){
+    console.log("3")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "3";
+}
+function yozplus(){
+    console.log("+")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "+";
+}
+function yozgg(){
+    console.log("+/-")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "+/-";
+}
+function yoz0(){
+    console.log("0")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "0";
+}
+function yoznuqta(){
+    console.log(".")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += ".";
+}
+function yozteng(){
+    console.log("=")
+    var inp1 =document.getElementById('inp1');
+    inp1.value += "=";
+}
